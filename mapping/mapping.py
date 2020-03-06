5777b73e3daaaf0b4be1d86129933fb8a023c1a9
Njoro_Mwangi <johnmwangielly@gmail.com>
2020-03-06 18:56:00 +0300

    using for loop for multiple markers

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -3,12 +3,9 @@ import folium
 map=folium.Map(location=[-1.371612, 36.719056], zoom_start=10, tiles="Mapbox Bright")\
 
 fg=folium.FeatureGroup(name="my map")
-fg.add_child(folium.Marker(location=[1.37, 36.719], popup="gataka", icon=folium.Icon(color='red')))
-fg.add_child(folium.Marker(location=[-0.435305, 36.959052], popup="nyeri", icon=folium.Icon(color='orange')))
-fg.add_child(folium.Marker(location=[-1.038653, 37.083425], popup="thika", icon=folium.Icon(color='black')))
-fg.add_child(folium.Marker(location=[-0.057283, 34.779527], popup="kisumu", icon=folium.Icon(color='green')))
-fg.add_child(folium.Marker(location=[-0.721058, 37.154714], popup="murang'a", icon=folium.Icon(color='blue')))
-fg.add_child(folium.Marker(location=[-0.275395, 36.377494], popup="olkalou", icon=folium.Icon(color='beige')))
+for coordinate in [[1.37, 36.719],[-0.435305, 36.959052],[-1.038653, 37.083425],[-0.057283, 34.779527],[-0.721058, 37.154714],[-0.275395, 36.377494]]:
+    fg.add_child(folium.Marker(location=[1.37, 36.719], popup="a town in kenya", icon=folium.Icon(color='red')))
+
 
 map.add_child(fg)
 
